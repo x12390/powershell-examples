9fee6d21703f5e452819ada018d18760971d51fe
Sven <send000999@googlemail.com>
2020-12-21 16:33:33 +0100

    Enable Winrm

---
+++ EnableWinrmBasicAuth.ps1
@@ -0,0 +1,6 @@
+﻿<#Winrm for Machine without AD. Enables Basic Authentication over http #>
+
+$enable = $true
+Enable-PSRemoting -Force
+Set-Item -Path WSMan:\localhost\Service\Auth\Basic -Value $enable
+Set-Item -Path WSMan:\localhost\Service\AllowUnencrypted -Value $enable
